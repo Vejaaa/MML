e3f785c06c02dad28929dcdef854137dd6091a7f
Veja Shetty <vejashetty@gmail.com>
2024-03-14 00:47:09 +0530

    Update prog3.py

--- prog3.py
+++ prog3.py
@@ -3,7 +3,9 @@ from sklearn.decomposition import PCA
 from PIL import Image
 
 #Load the image
-image = Image.open('WhatsApp Image 2024-02-28 at 13.10.02_e37bf7cb.jpg')
+image = Image.open('C:\\Users\\VEJASHETTY\\mml\\WhatsApp Image 2024-03-13 at 
+21.42.50_115e02f5.jpg')
+
 image = image.convert('L') #greyscale
 data = np.array(image)
 
@@ -14,4 +16,5 @@ reconstructed_data = pca.inverse_transform(compressed_data)
 
 #Save
 compressed_image = Image.fromarray(reconstructed_data.astype(np.uint8))
-compressed_image.save('Compressed_image.jpeg')
+compressed_image.save('C:\\Users\\VEJASHETTY\\mml\\WhatsApp Image 2024-03-13 at 
+21.42.50_115e02f5.jpg')
